0053c528676a53be250e448bda7dc1c59176686c
Guillermo Fernandez <midori@Mac-Pro.local>
2022-04-12 15:27:00 -0300

    footer&css

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from './footer.module.css'
+
+const Footer = () => {
+    return (
+        <div className={style.conteiner}>
+            <span className={style.span}>© 2022 Hubazar. All Rights Reserved</span>
+            <div className={style.div}>
+                <span className={style.span2}>Safe payment with:</span>
+                <img className={style.img} src='https://www.pinpng.com/pngs/m/424-4241166_mercado-pago-logo-png-volkswagen-fox-transparent-png.png' alt='payments' />
+            </div>
+        </div>
+    )
+}
+
+export default Footer
