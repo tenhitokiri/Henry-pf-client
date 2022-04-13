a704d4ab19588652989cd14ef3321e86ba8780c8
Guillermo Fernandez <midori@Mac-Pro.local>
2022-04-13 20:17:34 -0300

    fix footer & nav-50%

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,13 @@ import style from './footer.module.css'
 
 const Footer = () => {
     return (
-        <div className={style.container}>
-            <span className={style.span}>© 2022 Hubazar. All Rights Reserved</span>
-            <div className={style.div}>
-                <span className={style.span2}>Safe payment with:</span>
-                <img className={style.img} src='https://www.pinpng.com/pngs/m/424-4241166_mercado-pago-logo-png-volkswagen-fox-transparent-png.png' alt='payments' />
+        <div className={style.background}>
+            <div className={style.container}>
+                <span className={style.span}>© 2022 Hubazar. All Rights Reserved</span>
+                <div className={style.div}>
+                    <span className={style.span2}>Safe payment with:</span>
+                    <img className={style.img} src='https://www.pinpng.com/pngs/m/424-4241166_mercado-pago-logo-png-volkswagen-fox-transparent-png.png' alt='payments' />
+                </div>
             </div>
         </div>
     )
