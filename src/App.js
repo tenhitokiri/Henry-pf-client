48f36da085c9321bb0db25108a6da9d5361e3385
Christian Mejia <tenhitokiri@gmail.com>
2022-04-12 23:51:16 -0400

    started with some basic routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
