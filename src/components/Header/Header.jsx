80d90bc77ddde39fd800e9dd064b696f3a474bff
Juan Carlos Goicochea <juancgoicochea@gmail.com>
2022-04-15 09:25:59 -0500

    Show in the Header the amount of items in the Cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,9 +17,12 @@ const Header = () => {
             </div>
             <div className={styles.iconsContainer}>
                 <div className={styles.icons}>
-                <FontAwesomeIcon className={styles.icon} icon={faHeart} size="2x" />
-                <FontAwesomeIcon className={styles.icon} icon={faCartArrowDown} size="2x" />
-                <FontAwesomeIcon className={styles.icon} icon={faUser} size="2x" />
+                    <FontAwesomeIcon className={styles.icon} icon={faHeart} size="2x" />
+                    <div className={styles.cart}>
+                        <FontAwesomeIcon className={styles.icon} icon={faCartArrowDown} size="2x" />
+                        <span className={styles.cartItems}>2</span>
+                    </div>
+                    <FontAwesomeIcon className={styles.icon} icon={faUser} size="2x" />
                 </div>
                 <div className={styles.login}>
                     <div className={styles.loginLink}>Login in</div>
