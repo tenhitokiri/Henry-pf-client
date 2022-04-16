313dc857164594975365197b237b8a7b83898f4a
Christian Mejia <tenhitokiri@gmail.com>
2022-04-16 18:38:58 -0400

    cart working, not with localstorage

--- src/components/Cart/CartProduct.jsx
+++ src/components/Cart/CartProduct.jsx
@@ -3,30 +3,35 @@ import useCounter from '../../hooks/UseCounter';
 import { useDispatch } from 'react-redux'
 import { updateCartItem, removeFromCart } from '../../redux'
 import { FormatMoney } from 'format-money-js';
+import styles from './Cart.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faStar as starReg } from '@fortawesome/free-regular-svg-icons'
+import { faCircleMinus, faCircleXmark, faCirclePlus, faStar } from '@fortawesome/free-solid-svg-icons'
 
-function Product({ product, size }) {
+
+function CartProduct({ product, size }) {
     const {
-        handle, title,
-        variantInventoryQty,
-        variantPrice, imageSrc, itemsToBuy
+        id, title,
+        inventoryQty, price,
+        image, itemsToBuy
     } = product
 
     const formatMoney = new FormatMoney({ decimals: 2, symbol: '$', grouping: true })
-    const prodPrice = formatMoney.from(parseInt(variantPrice)) || variantPrice
+    const prodPrice = formatMoney.from(parseInt(price)) || price
 
     let newCount = itemsToBuy
     const dispatch = useDispatch()
     const updateCart = () => {
         const payload = {
-            handle,
-            variantPrice,
+            id,
+            price,
             itemsToBuy: newCount
         }
         dispatch(updateCartItem(payload))
     }
     const removeCartItem = () => {
         const payload = {
-            handle
+            id
         }
         dispatch(removeFromCart(payload))
     }
@@ -46,37 +51,40 @@ function Product({ product, size }) {
             newCount = 0
         }
         return (
-            <div className="ButtonGroup">
-                <button className="Button ButtonRegular" onClick={add} >+</button>
-                <div className="Button">
-                    <span className="Badge" >{count}</span>
+            <div className={styles.buttonGroup}>
+                <button className={`${styles.buttonAdd}`} onClick={add} ><FontAwesomeIcon icon={faCirclePlus} /></button>
+                <button className={`${styles.buttonRemove}`} onClick={remove} ><FontAwesomeIcon icon={faCircleMinus} /></button>
+
+                <div className={styles.buttonsStock}>
+
+                    <span className={styles.badge} >{count}</span>
                 </div>
-                <button className="Button ButtonRegular" onClick={remove} >-</button>
-                <button className="Button ButtonReset" onClick={resetCount} >reset</button>
+                <button className={`${styles.buttonReset}`} onClick={removeCartItem} ><FontAwesomeIcon icon={faCircleXmark} /></button>
+
             </div >
         )
     }
 
     return (
-        <div className={size === "medium" ? "Card2 Card2Medium" : "Card2 Card2Small"} key={handle} >
-            <div className="CardHeader"><h1>{title}</h1></div>
-            <div className="CardImage">
-                <img src={imageSrc} alt={handle} />
+        <div className={styles.cardItem} key={id} >
+            <div className={styles.cardHeader}>
+                <img src={image} alt={id} className={styles.img} />
             </div>
-            <div className="CardBody">
+            <div className={styles.title}><h1>{title}</h1></div>
+            <div className={styles.cardFooter}>
                 <p>
-                    Existencia: <span>{variantInventoryQty} Item(s)</span><br />
+                    Existencia: <span>{inventoryQty} Item(s)</span><br />
                 </p>
-                <div className="CardButton">
+                <div className={styles.cardFooter}>
                     <div className="CardPrice ">
                         Precio: <span> {prodPrice} </span>
                     </div>
-                    <button className="Button ButtonSuccess" onClick={updateCart}>cart</button>
-                    <Buttons initialCount={itemsToBuy} value={1} max={variantInventoryQty} />
-                    <button className="Button ButtonReset" onClick={removeCartItem}>rem</button>
+                    <Buttons initialCount={itemsToBuy} value={1} max={inventoryQty} />
+                    <button className={`${styles.buttonSuccess}`} onClick={updateCart}>Update cart</button>
+
                 </div>
             </div>
         </div>
     )
 }
-export default Product
+export default CartProduct
--- src/components/Products/ProductGetAll.jsx
+++ src/components/Products/ProductGetAll.jsx
@@ -4,20 +4,21 @@ import { connect } from 'react-redux'
 import { fetchProducts } from '../../redux'
 import ProductList from './ProductList'
 
-const ProductListAll = ({ fetchProducts, productList, loading, error }) => {
+const ProductListAll = ({ fetchProducts, productList, loading, error, cartList, numberOfItems }) => {
     useEffect(() => {
         fetchProducts()
         console.log(productList);
     }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
     const productMarkup = loading ? (
-        <div>Loading...</div>
+        <div className='App-container'>
+            <div className="loader"></div>
+        </div>
     ) : error ? (
         <div>{error}</div>
     ) : (
         <ProductList productList={productList} />
     )
-
 
     return (
         <div>
@@ -30,6 +31,8 @@ const mapStateToProps = state => ({
     productList: state.products.products,
     loading: state.products.loading,
     error: state.products.error,
+    cartList: state.cart.cartItems,
+    numberOfItems: state.cart.numberOfItems,
 })
 
 const mapDispatchToProps = dispatch => ({
