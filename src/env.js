706a8a8517754887b933a5f70c0e00b811ee6702
Guillermo Fernandez <midori@Mac-Pro.local>
2022-04-17 23:40:43 -0300

    products per page conflict

--- src/env.js
+++ src/env.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-export const backendUrl = process.env.API || "http://localhost:5000/"
-export const PRODUCTS_PER_PAGE = 9
-=======
 export const backendUrl = process.env.API || "https://fakestoreapi.com/"
-export const PRODUCTS_PER_PAGE = 6
->>>>>>> 47c6c1efdb20db3eb4e69693aae41bcc5287808b
+export const PRODUCTS_PER_PAGE = 9
