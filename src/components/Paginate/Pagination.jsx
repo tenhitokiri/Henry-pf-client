fff87bf0dc91bf16b3cc28671002e59e2724f9f9
Christian Mejia <tenhitokiri@gmail.com>
2022-04-17 20:16:18 -0400

    pagination done

---
+++ src/components/Paginate/Pagination.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import styles from './Paginate.module.css'
+
+const Pagination = ({ data, RenderComponent, pageLimit, dataLimit }) => {
+    const [pages] = useState(Math.round(data.length / dataLimit));
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const goToNextPage = () => {
+        setCurrentPage((page) => page + 1);
+    }
+
+    const goToPreviousPage = () => {
+        setCurrentPage((page) => page - 1);
+    }
+
+    const changePage = (event) => {
+        const pageNumber = Number(event.target.textContent);
+        setCurrentPage(pageNumber);
+    }
+
+    const getPaginatedData = () => {
+        const startIndex = currentPage * dataLimit - dataLimit;
+        const endIndex = startIndex + dataLimit;
+        return data.slice(startIndex, endIndex);
+    };
+
+    const getPaginationGroup = () => {
+        let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+        return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+    };
+
+    return (
+        <div>
+            <div className={styles.listContainer}>
+                {getPaginatedData().map((d, idx) => (
+                    <RenderComponent key={idx} product={d} />
+                ))}
+            </div>
+
+            <div className="pagination">
+                <button
+                    onClick={goToPreviousPage}
+                    className={`prev ${currentPage === 1 ? 'disabled' : ''}`}
+                >
+                    prev
+                </button>
+
+                {getPaginationGroup().map((item, index) => (
+                    <button
+                        key={index}
+                        onClick={changePage}
+                        className={`paginationItem ${currentPage === item ? 'active' : null}`}
+                    >
+                        <span>{item}</span>
+                    </button>
+                ))}
+
+                <button
+                    onClick={goToNextPage}
+                    className={`next ${currentPage === pages ? 'disabled' : ''}`}
+                >
+                    next
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Pagination
