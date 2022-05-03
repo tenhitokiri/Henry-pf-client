f67d321239f61215ea85c6b7d52d5004f1a76942
Christian Mejia <tenhitokiri@gmail.com>
2022-05-03 16:07:37 -0400

    Update env.js
    testing new env.js

--- src/env.js
+++ src/env.js
@@ -1,9 +1,5 @@
-
-export const backendUrl = process.env.API || 'http://localhost:5000/' //"https://hubazar.herokuapp.com/" // 
-export const mainPage = process.env.MAIN_PAGE || 'http://localhost:3000/'  //"https://hubazar.vercel.app/" // 
-
-
+export const backendUrl = process.env.NODE_ENV === "production" ? "https://hubazar.herokuapp.com/" : 'http://localhost:5000/'
+export const mainPage = process.env.NODE_ENV === "production" ? "https://hubazar.vercel.app/" : 'http://localhost:3000/'
 export const PRODUCTS_PER_PAGE = 6
-
 export const CLIENT_ID_GOOGLE = "780609260322-p4kck5h06qul2rnsfrhql4r03ueoh7lk.apps.googleusercontent.com"
 export const CLIENT_SECRET_GOOGLE = "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"
