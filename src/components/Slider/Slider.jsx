669f7814083eaf44402a0e347a8d5deb27cc2a32
Juan Carlos Goicochea <juancgoicochea@gmail.com>
2022-05-04 22:51:32 -0500

    Slider action to filter products

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import styles from '../Slider/Slider.module.css'
 import banner1 from "../../assets/banner-1.png";
 import banner2 from "../../assets/banner-2.png";
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { fetchProductByCategory } from '../../redux'
 
 const slideImages = [
     banner1,
@@ -11,17 +14,29 @@ const slideImages = [
   ]
 
 const Slider = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const onCategory = (e) => {
+    dispatch(fetchProductByCategory(e))
+    navigate("/find-product")   
+  }
+
     return (
-      <div>
+      <div className={styles.container}>
         <Slide easing="ease">
-          <div className={styles.eachSlide}>
-            <div style={{'backgroundImage': `url('${slideImages[0]}')`}}>
+          <a onClick={() => onCategory('Electronics')}>
+            <div className={styles.eachSlide}>
+              <div style={{'backgroundImage': `url('${slideImages[0]}')`}}>
+              </div>
             </div>
-          </div>
-          <div className={styles.eachSlide}>
-            <div style={{'backgroundImage': `url('${slideImages[1]}')`}}>
+          </a>
+          <a onClick={() => onCategory('Sports')}>
+            <div className={styles.eachSlide}>
+              <div style={{'backgroundImage': `url('${slideImages[1]}')`}}>
+              </div>
             </div>
-          </div>
+          </a>
         </Slide>
       </div>
     )
